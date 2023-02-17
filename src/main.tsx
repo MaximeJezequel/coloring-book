d56127f7e3dfb79c72d15cd5656ae446942a56a2
Maxime Jezequel <maxime.jezequel@gmail.com>
2023-02-17 02:12:40 +0100

    Test manifest + service worker offline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { registerSW } from "virtual:pwa-register"
 import App from "./App"
-import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW()
+}
